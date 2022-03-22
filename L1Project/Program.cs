9f6dbef9b6d0841ec3222dbec70399f98148a3bf
Harrison Jones <harjone@chbc.school.nz>
2022-03-23 09:35:08 +1300

    Added for loop that loops the testing method, and added a math method that gets the start germ count and takes away end germ count and divides by 30.

--- L1Project/Program.cs
+++ L1Project/Program.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿// Imports
+using System;
+using System.Collections.Generic;
 
 namespace ChemicalApp
 {
     class Program
     {
+        // Global variables
+        static List<int> chemicalName = new List<int>();
+        // Methods
+
+        // Tests the effiency of a chemical
+        // Component 2
+        static void ChemicalChoice()
+        {
+            Console.WriteLine("----------------------------------------------------\n" +
+                "Welcome to the chemical testing area. Please make a choice.\n" +
+                "----------------------------------------------------\n" +
+                "1. Hand Sanitiser\n" +
+                "2. Household Spray\n" +
+                "3. Chlorine\n" +
+                "4. Bleach\n");
+
+            decimal sumEfficiency;
+
+            for (int testCount = 1; testCount < 6; testCount++)
+            {
+                Console.WriteLine($"-------- Test {testCount} --------\n");
+                
+                Console.WriteLine("Enter chemical number.");
+                chemicalName.Add(Convert.ToInt32(Console.ReadLine()));
+
+                Console.WriteLine("Enter germ count.");
+
+                int startGermCount = Convert.ToInt32(Console.ReadLine());
+
+                Random rndm = new Random();
+
+                int endGermCount = rndm.Next(0, 150);
+
+                decimal chemicalEfficiency = (startGermCount - endGermCount) / 30;
+
+                chemicalEfficiency = Math.Round(chemicalEfficiency, 3);
+            }
+
+
+            
+                
+            
+                
+            
+            
 
 
 
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+        // Component 1
         // start or press run
         static void Main(string[] args)
         {
@@ -25,20 +87,20 @@ namespace ChemicalApp
                 string userChoice = Console.ReadLine();
                 if(userChoice.Equals("")) 
                 {
-                    Console.WriteLine("Testing Chemical");
+                    ChemicalChoice();
                 }
                 else if(userChoice.Equals("stop"))
                 {
                     flag_main = false;
-                    Console.WriteLine("Thank you for using HI-Jean chemical testing app");
+                    Console.WriteLine("Thank you for using the HI-Jean chemical testing app");
                 }
                 else
                 {
                     Console.WriteLine("ERROR: Please enter a correct choice");
                 }
-                    
-
                 
+
+
 
             }
 
